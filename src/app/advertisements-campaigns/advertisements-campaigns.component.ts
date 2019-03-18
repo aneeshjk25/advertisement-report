87862ad2f704d56f126279e16bc6ceb579771374
Aneesh Kallarakkal <kallarakkal@hotmail.com>
2019-03-18 13:10:07 +0100

    add drilldown

--- src/app/advertisements-campaigns/advertisements-campaigns.component.ts
+++ src/app/advertisements-campaigns/advertisements-campaigns.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, ElementRef } from '@angular/core';
 import { ReportsService } from '../reports.service';
 
 import * as Highcharts from 'highcharts';
+import Drilldown from 'highcharts/modules/drilldown';
+import { forkJoin } from 'rxjs';
+
 @Component({
   selector: 'app-advertisements-campaigns',
   templateUrl: './advertisements-campaigns.component.html',
@@ -12,32 +15,57 @@ export class AdvertisementsCampaignsComponent implements OnInit {
   constructor(private report: ReportsService, private el: ElementRef,
   ) { }
   Highcharts = Highcharts;
-  chartOptions = {
+  chartOptions: Highcharts.Options = {
     title: { text: 'Advertisers Campaign Expense' },
     yAxis: {
       title: {
         text: 'Euro €'
       }
+    },
+    chart: {
+      width: null,
     }
   };
 
   private chart: Highcharts.Chart;
 
   ngOnInit() {
-    this.chart = this.Highcharts.chart(this.el.nativeElement, this.chartOptions);
-
-    this
-      .report
-      .advertisersCampaigns()
-      .subscribe((advertisersCampaigns) => {
+    Drilldown(Highcharts);
+    forkJoin(
+      this.report.advertisersCampaigns(),
+      this.report.advertisersCampaignsReport()
+    )
+      .subscribe(([advertisersCampaigns, advertisersCampaignsReports]) => {
         advertisersCampaigns.forEach((advertisersCampaign) => {
           (advertisersCampaign as Highcharts.SeriesColumnDataOptions).y = advertisersCampaign.cost;
+          (advertisersCampaign as Highcharts.SeriesColumnDataOptions).drilldown = advertisersCampaign.name;
         });
-        this.chart.addSeries({
-          name: 'Expense',
+        const s = advertisersCampaigns.map((advertisersCampaign) => {
+          const series: Highcharts.SeriesColumnOptions = {
+            id: advertisersCampaign.name,
+            name: advertisersCampaign.name,
+            type: 'column',
+          };
+          const t = advertisersCampaignsReports
+            .filter((a) => a.advertiser_id === advertisersCampaign.advertiser_id)
+            .map((a) => {
+              const data = {
+                y: a.cost,
+                name: a.campaign_name
+              };
+              return data;
+            });
+          series.data = t;
+          return series;
+        });
+        this.chartOptions.drilldown = { series: s };
+
+        this.chartOptions.series = [{
           type: 'column',
-          data: advertisersCampaigns
-        });
+          data: advertisersCampaigns,
+          name: 'Expense',
+        }];
+        this.chart = this.Highcharts.chart(this.el.nativeElement, this.chartOptions);
       });
   }
 
--- src/app/reports.service.ts
+++ src/app/reports.service.ts
@@ -3,14 +3,18 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 const url = '/api';
 
-interface Advertiser {
-  id: number;
-  name: string;
-}
-export interface AdvertisersCampaigns {
+export interface AdvertisersCampaign {
   name: string;
   cost: number;
+  advertiser_id: number;
   number_of_campaigns: number;
+}
+export interface AdvertisersCampaignsReport {
+  advertiser_id: number;
+  campaign_id: number;
+  advertiser_name: string;
+  campaign_name: string;
+  cost: number;
 }
 
 @Injectable({
@@ -20,11 +24,11 @@ export class ReportsService {
 
   constructor(private http: HttpClient) { }
 
-  public advertisers() {
-    return this.http.get(url + '/advertisers') as Observable<Advertiser[]>;
+  public advertisersCampaigns() {
+    return this.http.get(url + '/advertisers_campaigns') as Observable<AdvertisersCampaign[]>;
   }
 
-  public advertisersCampaigns() {
-    return this.http.get(url + '/advertisers_campaigns') as Observable<AdvertisersCampaigns[]>;
+  public advertisersCampaignsReport() {
+    return this.http.get(url + '/advertisers_campaigns_reports') as Observable<AdvertisersCampaignsReport[]>;
   }
 }
