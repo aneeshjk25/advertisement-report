7b66009f8dad9ab03403b3e8a52c3ae2032e4b92
Aneesh Kallarakkal <kallarakkal@hotmail.com>
2019-03-19 10:57:08 +0100

    add proxy

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+var proxy = require('http-proxy-middleware')
 
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/advertisement-report'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/dist/advertisement-report/index.html'));
+app.use('/api', proxy({
+    target: 'https://aneesh-advertisement-report-bk.herokuapp.com', changeOrigin: true, pathRewrite: {
+        "^/api": ""
+    }
+}))
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '/dist/advertisement-report/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
